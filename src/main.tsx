9b89212508557bd25220506c49ef8a067fea02df
alessandro <alessandrobezerra100@gmail.com>
2026-02-27 16:24:51 -0300

    feat(front): fix purchase of an asset and list asset

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./Router.tsx"
 import "./index.css"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+            retry: false,
+        },
+    },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
         <Router/>
+        </QueryClientProvider>
     </React.StrictMode>
 );
