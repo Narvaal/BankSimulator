299733c85647aab2adf96c83cc3358041ea39ca3
alessandro <alessandrobezerra100@gmail.com>
2026-03-23 00:58:31 -0300

    feat(front): fix typos

--- src/component/util/CountdownTimer.tsx
+++ src/component/util/CountdownTimer.tsx
@@ -17,7 +17,7 @@ export default function CountdownTimer({targetDate}: CountdownTimerProps) {
         const minutes = Math.floor((difference / (1000 * 60)) % 60);
         const seconds = Math.floor((difference / 1000) % 60);
 
-        const formatted = `${String(hours).padStart(2, "0")}:${String(
+        const formatted = `${String(
             minutes
         ).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
 
