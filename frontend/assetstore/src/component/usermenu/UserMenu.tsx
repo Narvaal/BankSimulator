3471a7aa0513cdf82ae352168f954e4633589fcd
alessandro <alessandrobezerra100@gmail.com>
2026-03-27 20:43:20 -0300

    feat(front): fix ui

--- frontend/assetstore/src/component/usermenu/UserMenu.tsx
+++ frontend/assetstore/src/component/usermenu/UserMenu.tsx
@@ -57,7 +57,7 @@ export default function Navbar({ balance, nextFreeAssetAt, name, imageUrl }: Acc
                             </div>
 
                             <div className="flex items-stretch border-l border-emerald-200">
-                              <div className="flex items-center px-3 hover:bg-emerald-100 active:scale-95 transition">
+                              <div className="flex items-center px-3 h-8 active:scale-95 transition">
                                 <HeartIcon className="w-4 h-4 text-emerald-600" />
                               </div>
                             </div>
